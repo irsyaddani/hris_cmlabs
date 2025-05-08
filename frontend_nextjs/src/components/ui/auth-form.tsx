085cca75f52eef18c30199954e6729a5d2d89fb9
ptryuwana <137630717+ptryuwana@users.noreply.github.com>
2025-05-08 16:32:02 +0700

    update: auth-form

--- frontend_nextjs/src/components/ui/auth-form.tsx
+++ frontend_nextjs/src/components/ui/auth-form.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { PasswordInput } from "./password-input";
-
+import { IconUserCircle } from "@tabler/icons-react";
 import React from "react";
 
 import axios from "axios";
