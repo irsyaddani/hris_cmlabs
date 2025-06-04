25549ed6e8d05c37484df21f664e9b3357e58e40
Dennis Parulian Panjaitan <drekodraco1@gmail.com>
2025-06-04 20:47:33 +0700

    feat : menambah ClockSettingsController.php

--- backend_laravel/routes/api.php
+++ backend_laravel/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\CheckClockController;
+use App\Http\Controllers\API\ClockSettingsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,6 +48,8 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
 });
-
+// routes/api.php
 Route::get('/checkclocks', [CheckClockController::class, 'index']);
+Route::post('/clock-settings', [ClockSettingsController::class, 'store']);
+
 
