3a109c202a9c1d7fd51cffb8fe0ae00b728cfa45
amandafadilaerros <amandafadila1@mail.com>
2025-06-04 15:58:14 +0700

    fix detail checklock

--- frontend_nextjs/src/app/checkclock/page.tsx
+++ frontend_nextjs/src/app/checkclock/page.tsx
@@ -1,67 +1,99 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { DataTable } from "../../components/data-table-components/data-table";
 import { columns } from "../../components/data-table-components/columns-checkclock";
 
 interface CheckClock {
+  id: number;
   name: string;
-  avatarUrl: string;
+  avatarUrl?: string;
   position: string;
   clockIn: string | null;
   clockOut: string | null;
   workHours: number;
   approval: string;
   status: string;
+  reason?: string;
+  proofFile?: {
+    fileName: string;
+    fileUrl: string;
+    fileType: string;
+  } | null;
+  startDate?: string;
+  endDate?: string;
 }
 
 export default function CheckClockPage() {
+  const searchParams = useSearchParams();
+  const idParam = searchParams.get("id");
+  const numericId = idParam ? parseInt(idParam, 10) : undefined;
+
   const [data, setData] = useState<CheckClock[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    async function fetchCheckClocks() {
-      try {
-        const response = await fetch("http://localhost:8000/api/checkclocks");
+  async function fetchCheckClocks() {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:8000/api/checkclock");
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = await response.json();
-
-        if (!result.data || !Array.isArray(result.data)) {
-          throw new Error("Format data API tidak sesuai");
-        }
-
-        setData(result.data);
-      } catch (error: any) {
-        console.error("Gagal mengambil data check clock", error);
-        setError(error.message || "Terjadi kesalahan saat mengambil data");
-      } finally {
-        setLoading(false);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-    }
 
+      const result = await response.json();
+
+      if (!result.data || !Array.isArray(result.data)) {
+        throw new Error("Format data API tidak sesuai");
+      }
+
+      setData(result.data);
+      setError(null);
+    } catch (error: any) {
+      console.error("Gagal mengambil data check clock", error);
+      setError(error.message || "Terjadi kesalahan saat mengambil data");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateApproval(id: string, status: string) {
+    try {
+      const response = await fetch(`http://localhost:8000/api/checkclock/approval/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ approvalStatus: status }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Server response error:", errorData);
+        throw new Error("Failed to update approval");
+      }
+
+      const data = await response.json();
+      console.log("Approval updated successfully:", data);
+    } catch (error: any) {
+      console.error("Fetch failed:", error.message);
+      alert("Gagal mengupdate approval. Silakan coba lagi.");
+    }
+  }
+
+  useEffect(() => {
     fetchCheckClocks();
   }, []);
 
-  if (loading) {
-    return <div className="p-6">Loading...</div>;
-  }
-
-  if (error) {
-    return <div className="p-6 text-red-600">Error: {error}</div>;
-  }
-
-  if (data.length === 0) {
-    return <div className="p-6">Tidak ada data check clock.</div>;
-  }
-
   return (
     <div className="min-h-[100vh] flex flex-col flex-1 p-6 gap-7">
-      <DataTable data={data} columns={columns} toolbarVariant="checkclock" />
+      <DataTable
+        data={data}
+        columns={columns(updateApproval)}
+        toolbarVariant="checkclock"
+        loading={loading}
+        error={error}
+      />
     </div>
   );
 }
--- frontend_nextjs/src/components/data-table-components/columns-checkclock.tsx
+++ frontend_nextjs/src/components/data-table-components/columns-checkclock.tsx
@@ -9,259 +9,262 @@ import { Button } from "@/components/ui/button";
 import { IconCheck, IconX } from "@tabler/icons-react";
 import { ConfirmDialog } from "../dialogs/confirm-dialog";
 
-export const columns: ColumnDef<Checkclock>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="translate-y-0.5"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="translate-y-0.5"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "name",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Name" />
-    ),
-    cell: ({ row }) => {
-      const name = row.getValue("name") as string;
-      //   avatar
-      //   const avatarUrl = row.original.avatarUrl as string | undefined;
-
-      return (
-        <div className="flex items-center gap-2">
-          {/* <img
-            src={avatarUrl || "/default-avatar.png"}
-            alt={name}
-            className="w-7 h-7 rounded-full object-cover"
-          /> */}
-          <div className="w-6 h-6 rounded-full bg-blue-500" />
-          <span className="capitalize">{name}</span>
-        </div>
-      );
+export function columns(
+  updateApproval: (id: string, status: string) => void
+): ColumnDef<Checkclock>[] {
+  return [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+          className="translate-y-0.5"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+          className="translate-y-0.5"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
     },
-  },
-  {
-    accessorKey: "position",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Position" />
-    ),
-    cell: ({ row }) => <div>{row.getValue("position") as string}</div>,
-  },
-  {
-    accessorKey: "clockIn",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Clock In" />
-    ),
-    cell: ({ row }) => {
-      const status = row.getValue("status") as string;
-      const value = row.getValue("clockIn") as string | undefined;
-      const clockOut = row.getValue("clockOut") as string | undefined;
-
-      if (status === "permit" || status === "annual leave" || !value) {
-        if (!value && !clockOut) return <div>-</div>; // no-show
-        return <div>-</div>;
-      }
-
-      const datetime = new Date(value);
-      return (
-        <div>
-          {datetime.toLocaleTimeString("id-ID", {
-            hour: "2-digit",
-            minute: "2-digit",
-            timeZone: "Asia/Jakarta",
-          })}
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "clockOut",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Clock Out" />
-    ),
-    cell: ({ row }) => {
-      const status = row.getValue("status") as string;
-      const clockOut = row.getValue("clockOut") as string | undefined;
-      const clockIn = row.getValue("clockIn") as string | undefined;
-
-      if (
-        status === "permit" ||
-        status === "annual leave" ||
-        (!clockOut && !clockIn)
-      ) {
-        return <div>-</div>;
-      }
-
-      if (!clockOut) return <div>Awaiting</div>;
-
-      const clockOutDate = new Date(clockOut);
-      const isAwaiting =
-        clockOutDate.getUTCHours() === 0 && clockOutDate.getUTCMinutes() === 0;
-
-      return (
-        <div>
-          {isAwaiting
-            ? "awaiting"
-            : clockOutDate.toLocaleTimeString("id-ID", {
-                hour: "2-digit",
-                minute: "2-digit",
-                timeZone: "Asia/Jakarta",
-              })}
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "workHours",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Work Hours" />
-    ),
-    cell: ({ row }) => {
-      const status = row.getValue("status") as string;
-      const clockIn = row.getValue("clockIn") as string | undefined;
-      const clockOut = row.getValue("clockOut") as string | undefined;
-
-      if (
-        status === "permit" ||
-        status === "annual leave" ||
-        (!clockIn && !clockOut)
-      ) {
-        return <div>-</div>;
-      }
-
-      let hours = row.getValue("workHours") as number;
-
-      if (!clockOut && clockIn) {
-        const start = new Date(clockIn);
-        const now = new Date();
-        const diffMs = now.getTime() - start.getTime();
-        hours = parseFloat((diffMs / 1000 / 60 / 60).toFixed(2)); // format ke 2 desimal
-      }
-
-      return <div>{`${hours} hrs`}</div>;
-    },
-  },
-  {
-    accessorKey: "approval",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Approval" />
-    ),
-    cell: ({ row }) => {
-      const status = row.getValue("status") as string;
-      const approval = row.getValue("approval") as string;
-
-      if (status === "permit" || status === "annual leave") {
-        if (approval === "pending") {
-          return (
-            <div className="flex gap-2">
-              <ConfirmDialog
-                trigger={
-                  <Button size="icon" variant="outline">
-                    <IconCheck className="h-4 w-4" />
-                  </Button>
-                }
-                title="Are you sure want to proceed?"
-                description="This action cannot be undone. This will permanently update to employee data."
-                confirmText="Approve"
-                cancelText="Cancel"
-                confirmClassName="bg-[var(--color-primary-900)] text-white hover:bg-[var(--color-primary-800)]"
-                onConfirm={() => console.log(`Approved: ${row.getValue("id")}`)}
-              />
-              <ConfirmDialog
-                trigger={
-                  <Button size="icon" variant="outline">
-                    <IconX className="h-4 w-4" />
-                  </Button>
-                }
-                title="Are you sure want to proceed?"
-                description="This action cannot be undone. This will permanently update to employee data."
-                confirmText="Rejected"
-                cancelText="Cancel"
-                confirmClassName="bg-[var(--color-danger-main)] text-white hover:bg-[var(--color-danger-hover)]"
-                onConfirm={() => console.log(`Approved: ${row.getValue("id")}`)}
-              />
-            </div>
-          );
-        }
+    {
+      accessorKey: "name",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Name" />
+      ),
+      cell: ({ row }) => {
+        const name = row.getValue("name") as string;
         return (
-          <div
-            className={`inline-flex items-center gap-2 px-2 py-1 rounded-full text-sm font-medium capitalize
-              ${
+          <div className="flex items-center gap-2">
+            <div className="w-6 h-6 rounded-full bg-blue-500" />
+            <span className="capitalize">{name}</span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "position",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Position" />
+      ),
+      cell: ({ row }) => <div>{row.getValue("position") as string}</div>,
+    },
+    {
+      accessorKey: "clockIn",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Clock In" />
+      ),
+      cell: ({ row }) => {
+        const status = row.getValue("status") as string;
+        const value = row.getValue("clockIn") as string | undefined;
+        const clockOut = row.getValue("clockOut") as string | undefined;
+
+        if (status === "permit" || status === "annual leave" || !value) {
+          if (!value && !clockOut) return <div>-</div>;
+          return <div>-</div>;
+        }
+
+        const datetime = new Date(value);
+        return (
+          <div>
+            {datetime.toLocaleTimeString("id-ID", {
+              hour: "2-digit",
+              minute: "2-digit",
+              timeZone: "Asia/Jakarta",
+            })}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "clockOut",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Clock Out" />
+      ),
+      cell: ({ row }) => {
+        const status = row.getValue("status") as string;
+        const clockOut = row.getValue("clockOut") as string | undefined;
+        const clockIn = row.getValue("clockIn") as string | undefined;
+
+        if (
+          status === "permit" ||
+          status === "annual leave" ||
+          (!clockOut && !clockIn)
+        ) {
+          return <div>-</div>;
+        }
+
+        if (!clockOut) return <div>Awaiting</div>;
+
+        const clockOutDate = new Date(clockOut);
+        const isAwaiting =
+          clockOutDate.getUTCHours() === 0 &&
+          clockOutDate.getUTCMinutes() === 0;
+
+        return (
+          <div>
+            {isAwaiting
+              ? "awaiting"
+              : clockOutDate.toLocaleTimeString("id-ID", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  timeZone: "Asia/Jakarta",
+                })}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "workHours",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Work Hours" />
+      ),
+      cell: ({ row }) => {
+        const status = row.getValue("status") as string;
+        const clockIn = row.getValue("clockIn") as string | undefined;
+        const clockOut = row.getValue("clockOut") as string | undefined;
+
+        if (
+          status === "permit" ||
+          status === "annual leave" ||
+          (!clockIn && !clockOut)
+        ) {
+          return <div>-</div>;
+        }
+
+        const workHours = row.getValue("workHours") as {
+          hours: number;
+          minutes: number;
+        };
+
+        const { hours, minutes } = workHours;
+
+        if (!clockOut && clockIn) {
+          const start = new Date(clockIn);
+          const now = new Date();
+          const diffMs = now.getTime() - start.getTime();
+          const totalMinutes = Math.floor(diffMs / 1000 / 60);
+          const dynamicHours = Math.floor(totalMinutes / 60);
+          const dynamicMinutes = totalMinutes % 60;
+
+          return <div>{`${dynamicHours} hrs ${dynamicMinutes} mins`}</div>;
+        }
+
+        return <div>{`${hours}.${minutes} hrs`}</div>;
+      },
+    },
+    {
+      accessorKey: "approval",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Approval" />
+      ),
+      cell: ({ row }) => {
+        const status = row.getValue("status") as string;
+        const approval = row.getValue("approval") as string;
+        // Ganti row.getValue("id") dengan row.original.id
+        const id = row.original.id as string;
+
+        if (status === "permit" || status === "annual leave") {
+          if (approval === "pending") {
+            return (
+              <div className="flex gap-2">
+                <ConfirmDialog
+                  trigger={
+                    <Button size="icon" variant="outline">
+                      <IconCheck className="h-4 w-4" />
+                    </Button>
+                  }
+                  title="Are you sure want to proceed?"
+                  description="This action cannot be undone. This will permanently update to employee data."
+                  confirmText="Approve"
+                  cancelText="Cancel"
+                  confirmClassName="bg-[var(--color-primary-900)] text-white hover:bg-[var(--color-primary-800)]"
+                  onConfirm={() => updateApproval(id, "approved")}
+                />
+                <ConfirmDialog
+                  trigger={
+                    <Button size="icon" variant="outline">
+                      <IconX className="h-4 w-4" />
+                    </Button>
+                  }
+                  title="Are you sure want to proceed?"
+                  description="This action cannot be undone. This will permanently update to employee data."
+                  confirmText="Reject"
+                  cancelText="Cancel"
+                  confirmClassName="bg-[var(--color-danger-main)] text-white hover:bg-[var(--color-danger-hover)]"
+                  onConfirm={() => updateApproval(id, "rejected")}
+                />
+              </div>
+            );
+          }
+
+          return (
+            <div
+              className={`inline-flex items-center gap-2 px-2 py-1 rounded-full text-sm font-medium capitalize ${
                 approval === "approved"
                   ? "bg-green-100 text-black"
                   : approval === "rejected"
                   ? "bg-[var(--color-neutral-100)] text-black"
                   : ""
               }`}
-          >
-            <div
-              className={`flex items-center justify-center w-4 h-4 rounded-full
-                ${
+            >
+              <div
+                className={`flex items-center justify-center w-4 h-4 rounded-full ${
                   approval === "approved"
                     ? "bg-green-500 text-white"
                     : "bg-[var(--color-neutral-500)] text-white"
                 }`}
-            >
-              {approval === "approved" ? (
-                <IconCheck className="w-3 h-3" />
-              ) : (
-                <IconX className="w-3 h-3" />
-              )}
+              >
+                {approval === "approved" ? (
+                  <IconCheck className="w-3 h-3" />
+                ) : (
+                  <IconX className="w-3 h-3" />
+                )}
+              </div>
+              {approval}
             </div>
-            {approval}
-          </div>
+          );
+        }
+
+        return <div>-</div>;
+      },
+    },
+    {
+      accessorKey: "status",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Status" />
+      ),
+      cell: ({ row }) => {
+        const status = row.getValue("status") as string;
+        const statusStyles: Record<string, string> = {
+          "on time": "text-green-600 bg-green-100",
+          late: "text-yellow-600 bg-yellow-100",
+          permit: "text-purple-600 bg-purple-100",
+          "annual leave": "text-blue-600 bg-blue-100",
+          "no-show": "text-red-600 bg-red-100",
+        };
+        return (
+          <span
+            className={`px-2 py-1 rounded-full text-sm font-medium capitalize ${statusStyles[status]}`}
+          >
+            {status}
+          </span>
         );
-      }
-
-      return <div>-</div>;
+      },
     },
-  },
-  {
-    accessorKey: "status",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Status" />
-    ),
-    cell: ({ row }) => {
-      const status = row.getValue("status") as string;
-      const clockIn = row.getValue("clockIn") as string | undefined;
-      const clockOut = row.getValue("clockOut") as string | undefined;
-
-      const finalStatus = !clockIn && !clockOut ? "no-show" : status;
-      const statusStyles: Record<string, string> = {
-        "on time": "text-green-600 bg-green-100",
-        late: "text-yellow-600 bg-yellow-100",
-        permit: "text-purple-600 bg-purple-100",
-        "annual leave": "text-blue-600 bg-blue-100",
-        "no-show": "text-red-600 bg-red-100",
-      };
-      return (
-        <span
-          className={`px-2 py-1 rounded-full text-sm font-medium capitalize ${statusStyles[status]}`}
-        >
-          {status}
-        </span>
-      );
+    {
+      id: "actions",
+      cell: ({ row }) => <DataTableRowActions row={row} variant="checkclock" />,
     },
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <DataTableRowActions row={row} variant="checkclock" />,
-  },
-];
+  ];
+}
